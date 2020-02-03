1108416551d0e027ddc6a4c9cd1de4eeabed92f2
segawa <>
2020-02-03 13:33:26 +0900

    TypeScriptにして一応うごくところまで

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-function Drk7jpPollen() { }
-callback(json);
-{
-    console.log(json);
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        xhr.open(method, url, true);
+    }
+    else if (typeof XDomainRequest != "undefined") {
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    }
+    else {
+        xhr = null;
+    }
+    return xhr;
 }
-;
+var submitZipCode = document.getElementById("js-submit");
+submitZipCode.onclick = function () {
+    var inputZipCode = document.getElementById("js-zipcode");
+    var inputPref = document.getElementById("js-pref");
+    var inputAddress = document.getElementById("js-address");
+    var xhr = createCORSRequest('GET', 'https://api.zipaddress.net/?zipcode=' + inputZipCode.value);
+    if (!xhr) {
+        throw new Error('CORS not supported');
+    }
+    xhr.onload = function () {
+        var responseArr = JSON.parse(xhr.response);
+        inputPref.value = responseArr.data.pref;
+        inputAddress.value = responseArr.data.address;
+    };
+    xhr.onerror = function (error) {
+        console.log(error);
+    };
+    xhr.send();
+};
 //# sourceMappingURL=app.js.map
